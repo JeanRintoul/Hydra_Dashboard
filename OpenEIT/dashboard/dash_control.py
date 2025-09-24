3c1233fa09c04f12d3a27b7f8f28da72707d820b
Jean Rintoul <jean.rintoul@gmail.com>
2025-09-24 21:06:50 +0100

    change

--- OpenEIT/dashboard/dash_control.py
+++ OpenEIT/dashboard/dash_control.py
@@ -7,8 +7,11 @@
 import dash 
 import logging
 from dash.dependencies import Input, Output
-import dash_html_components as html
-import dash_core_components as dcc
+# import dash_html_components as html
+# import dash_core_components as dcc
+from dash import html
+from dash import dcc
+
 from . import page_not_found
 from . import state 
 from .modes import mode_names
